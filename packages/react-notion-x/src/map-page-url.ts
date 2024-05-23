0716e4c121e740f178a8e1c3fa37062fd30e34b0
Rimon Hanna <rimon.hanna@live.com>
2024-05-23 14:47:46 +0200

    Update map-page-url.ts

--- packages/react-notion-x/src/map-page-url.ts
+++ packages/react-notion-x/src/map-page-url.ts
@@ -2,8 +2,8 @@ export const defaultMapPageUrl = (rootPageId?: string) => (pageId: string) => {
   pageId = (pageId || '').replace(/-/g, '')
 
   if (rootPageId && pageId === rootPageId) {
-    return '/'
+    return window.location.href
   } else {
-    return `/${pageId}`
+    return `${window.location.href}/${pageId}`
   }
 }
